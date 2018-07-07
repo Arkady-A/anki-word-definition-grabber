1899158be52423ae81bf1f6ddd42f92829250526
Arkady-M <arkadymirz@gmail.com>
2018-07-07 16:57:29 +0300

    basic structure

---
+++ data_collectors/dictionarycollector.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+
+class Dictionary_Collector():
+    self.__data__ = {}
+
+    def __init__(self):
+        return ("I'm father collector")
+
+    def get_data():
+        '''Returns dictionary with data'''
+        return self.data
+
+
+class Oxford_Word_Collector(Dictionary_Collector):
+    def __init__(self):
+        return 0
+
+
+class Cambridge_Word_Collector(Dictionary_Collector):
+    def __init__(self):
+        return 0
+
+
+class Dicionary_com_Word_Collector(Dictionary_Collector):
+    def __init__(self):
+        return 0
---
+++ start.py
@@ -0,0 +1,5 @@
+import data_collectors.dictionarycollector as dicts_col
+import representers
+
+if __name__ == '__main__':
+    print('Hello console!')
