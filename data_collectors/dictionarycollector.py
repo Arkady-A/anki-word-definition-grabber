36b1c50df02ec7b13b5ba3a6af6c1782651ff0f0
Arkady-M <arkadymirz@gmail.com>
2018-07-08 13:50:26 +0300

    basic structure for oxford word collector

--- data_collectors/dictionarycollector.py
+++ data_collectors/dictionarycollector.py
@@ -1,21 +1,52 @@
 from bs4 import BeautifulSoup
+import requests
 
 
 class Dictionary_Collector():
-    self.__data__ = {}
 
     def __init__(self):
-        return ("I'm father collector")
-
-    def get_data():
+        self.__data__= { # setting structure for data # TODO place that structure higher
+            'source':None,
+            'type':None,
+            'data':[
+                
+                ]
+        }
+        self.__process_structure__={
+            'word':None,
+            'part_of_speech':None,
+            'Definition':[],
+        }
+    
+    def __load_data__(self,link, word):
+        self.raw_html = requests.get('{}/{}'.format(link,word)).text
+        self.full_link = '{}/{}'.format(link,word) # full link to page
+        
+    def get_data(self):
         '''Returns dictionary with data'''
-        return self.data
+        return self.__data__
 
 
 class Oxford_Word_Collector(Dictionary_Collector):
     def __init__(self):
-        return 0
+        super().__init__()
+        
+        self.link = 'https://en.oxforddictionaries.com/definition'
+        self.__data__['type'] = 'word'
+        
+    def __process_data__(self, word):
+        parser = BeautifulSoup(self.raw_html,'html.parser')
+        sections = parser.find_all('section', 'gramb')
 
+        for section in list(sections):
+            self.__process_structure__['word'] = word
+            self.__data__['data'].append(self.__process_structure__)
+
+    def get_data(self, word):
+        self.__load_data__(self.link,word)
+        self.__data__['source']= self.full_link
+        self.__process_data__(word)
+        return self.__data__
 
 class Cambridge_Word_Collector(Dictionary_Collector):
     def __init__(self):
--- start.py
+++ start.py
@@ -1,5 +1,9 @@
 import data_collectors.dictionarycollector as dicts_col
 import representers
+from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     print('Hello console!')
+    test = dicts_col.Oxford_Word_Collector()
+    print(test.get_data('home'))
+#    print(test.raw_html)
