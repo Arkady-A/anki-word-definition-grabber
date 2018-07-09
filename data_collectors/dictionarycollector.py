3fac015a2f7ecebc0cfaaa376df1797f6d2df1aa
Arkady-M <arkadymirz@gmail.com>
2018-07-09 09:15:12 +0300

    Architecture changes data/definition classes added

--- data_collectors/dictionarycollector.py
+++ data_collectors/dictionarycollector.py
@@ -2,26 +2,36 @@ from bs4 import BeautifulSoup
 import requests
 
 
+class Data():                   # TODO put this class higher in different module
+    def __init__(self):
+        self.source = None      # type: String
+        self.data_type = None   # type: String
+        self.data = []        # type: List of Definition's
+
+
+class Definition():
+    def __init__(self):
+        self.word = None        # type: String
+        self.part_of_speech = None  # type: String
+        self.definitions = None  # type: String
+        self.examples = []  # type: List of strings
+
+    def get_data(self):
+        data = {
+            'word': self.word,
+            'part_of_speech': self.part_of_speech,
+            'definition': self.definitions,
+            'examples': self.examples,
+        }
+        return data
+
+
 class Dictionary_Collector():
 
-    def __init__(self):
-        self.__data__= { # setting structure for data # TODO place that structure higher
-            'source':None,
-            'type':None,
-            'data':[
-                
-                ]
-        }
-        self.__process_structure__={
-            'word':None,
-            'part_of_speech':None,
-            'Definition':[],
-        }
-    
-    def __load_data__(self,link, word):
-        self.raw_html = requests.get('{}/{}'.format(link,word)).text
-        self.full_link = '{}/{}'.format(link,word) # full link to page
-        
+    def __load_data__(self, link, word):
+        self.raw_html = requests.get('{}/{}'.format(link, word)).text
+        self.full_link = '{}/{}'.format(link, word)  # full link to page
+
     def get_data(self):
         '''Returns dictionary with data'''
         return self.__data__
@@ -30,23 +40,37 @@ class Dictionary_Collector():
 class Oxford_Word_Collector(Dictionary_Collector):
     def __init__(self):
         super().__init__()
-        
-        self.link = 'https://en.oxforddictionaries.com/definition'
-        self.__data__['type'] = 'word'
-        
-    def __process_data__(self, word):
-        parser = BeautifulSoup(self.raw_html,'html.parser')
-        sections = parser.find_all('section', 'gramb')
 
-        for section in list(sections):
-            self.__process_structure__['word'] = word
-            self.__data__['data'].append(self.__process_structure__)
+        self.link = 'https://en.oxforddictionaries.com/definition'
+        self.data = Data()
+
+    def __process_data__(self, word):
+        parser = BeautifulSoup(self.raw_html, 'html.parser')
+        sections = parser.find('div', 'entryWrapper').find_all(
+            'section', 'gramb', recursive=False)
+
+        for counter, section in enumerate(list(sections)):
+            print("section!{}".format(counter))
+            definitions = section.find(
+                'ul', 'semb').find_all('li', recursive=False)
+            for definition in list(definitions):
+               # print(definition)
+                definition_var = Definition()
+                definition_var.word = word
+                definition_var.part_of_speech = section.find(
+                    'span', 'pos').get_text()
+                definition_var.definition = definition.find('p').get_text(),
+                definition_var.examples = [
+                    x.get_text() for x in definition.find_all('div', 'ex', limit=3)]
+                self.data.data.append(definition_var)
 
     def get_data(self, word):
-        self.__load_data__(self.link,word)
-        self.__data__['source']= self.full_link
+        self.__load_data__(self.link, word)
+        self.data.source = self.full_link
+        self.data.data_type = 'word'
         self.__process_data__(word)
-        return self.__data__
+        return self.data
+
 
 class Cambridge_Word_Collector(Dictionary_Collector):
     def __init__(self):
--- start.py
+++ start.py
@@ -1,9 +1,14 @@
 import data_collectors.dictionarycollector as dicts_col
 import representers
 from bs4 import BeautifulSoup
+import pprint
 
 if __name__ == '__main__':
     print('Hello console!')
     test = dicts_col.Oxford_Word_Collector()
-    print(test.get_data('home'))
+    thing = test.get_data('home')
+    pprint.pprint('The thing is {}'.format(thing))
+    for defin in thing.data:
+        print(defin.get_data())
+ #   print(test.get_data('home'))
 #    print(test.raw_html)
